4339eee09371d3804bc7b2d18b3ea0b39b40b055
Brandon Cheng <bc323@duke.edu>
2026-02-27 19:38:37 -0500

    Basic template and folders

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <h1>Welcome to Chameleon</h1>
+      <p>This is a basic React frontend template.</p>
+    </div>
+  );
+}
---
+++ frontend/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    historyApiFallback: true,
+    port: 3000,
+  },
+};
